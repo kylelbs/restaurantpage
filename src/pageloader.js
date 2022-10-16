3b94b4a65ce1691029b87a2cbc9cfaba2c8ce3ea
kylelbs <kyle.lebrissaget@gmail.com>
2022-10-16 21:15:58 +0200

    removed html content and intialized a js pageloader

---
+++ src/pageloader.js
@@ -0,0 +1,17 @@
+/* <div id="content">
+<nav>
+  <ul>
+    <li>About</li>
+    <li>Reservations</li>
+    <li>Menus</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+<h1>Welcome to the Webpack restaurant</h1>
+<p>
+  The webpack restaurant is a gatronomic restaurant that serves mainly
+  pure air dishes. From the vacuum.
+</p>
+<button>Book a table (actually you can't)</button>
+</div> */
+
