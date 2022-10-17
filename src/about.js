261d29e6d1268911d551e444a4dd8e926194ba5e
kylelbs <kyle.lebrissaget@gmail.com>
2022-10-17 22:31:12 +0200

    created base layout with DOM for each page

---
+++ src/about.js
@@ -0,0 +1,9 @@
+const about = (() => {
+    const divContent = document.querySelector('#content');
+    const mainParagraph = document.createElement('p');
+    mainParagraph.innerText = 'This is the about page';
+})();
+
+export {about};
+    
+    
