4adf58570b7b6ad06425fc68bc9a9b4b47d06ac1
kylelbs <kyle.lebrissaget@gmail.com>
2022-10-17 22:03:46 +0200

    added base query selection on links in the index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import { pageloader } from "./pageloader";
 
+const aboutLink = document.querySelector('ul :nth-child(1)');
+const menuLink = document.querySelector('ul :nth-child(2)');
+const contactLink = document.querySelector('ul :nth-child(3)');
+
+aboutLink.addEventListener('click', ()=> {
+    aboutLink.innerHTML = 'clicked!';
+});
